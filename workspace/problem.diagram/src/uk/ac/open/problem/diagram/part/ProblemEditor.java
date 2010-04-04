7ca896cfbccbe2fae25835125ec89b4a6373a507
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-04-04 21:28:05 +0000

    Make sure when the text PF file is saved, both the EMF model and GMF diagrams are automatically created. (There is no need to right click to initialise the diagram from EMF model now).
    The risk of this is that the manual change to the diagram
    might get lost when the text file is saved again. So please
    rename the manually edited diagram to avoid this conflict.
    Maybe do not overwrite if the diagram file already exists,
    
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@40 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- workspace/problem.diagram/src/uk/ac/open/problem/diagram/part/ProblemEditor.java
+++ workspace/problem.diagram/src/uk/ac/open/problem/diagram/part/ProblemEditor.java
@@ -1,8 +1,9 @@
-package problem;
+package uk.ac.open.problem.diagram.part;
 
 import java.io.IOException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.resource.XtextResource;
@@ -11,23 +12,35 @@ import org.eclipse.xtext.ui.core.editor.XtextEditor;
 
 import uk.ac.open.ProblemStandaloneSetup;
 
+/**
+ * 
+ * @author yy66
+ * @generate NOT
+ */
 public class ProblemEditor extends XtextEditor {
 
 	@Override
-	public void performSave(boolean overwrite, IProgressMonitor monitor)
-	{
+	public void performSave(boolean overwrite, IProgressMonitor monitor) {
 		super.performSave(overwrite, monitor);
 		String filename = this.getResource().getFullPath().toOSString();
 		// http://www.eclipse.org/forums/index.php?t=msg&goto=520616&
-		ProblemStandaloneSetup.doSetup();		
+		ProblemStandaloneSetup.doSetup();
 		XtextResourceSet resourceSet = new XtextResourceSet();
 		// http://www.eclipse.org/forums/index.php?t=msg&goto=480679&
-		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL,
+				Boolean.TRUE);
 		URI uri = URI.createURI(filename); // your input textual file
-		Resource xtextResource = resourceSet.getResource(uri , true);
-		String newfile = filename.substring(0, filename.lastIndexOf("."))+ ".problem";
-		Resource xmiResource = resourceSet.createResource(URI.createURI(newfile));
+		Resource xtextResource = resourceSet.getResource(uri, true);
+		String newfile = filename.substring(0, filename.lastIndexOf("."))
+				+ ".problem";
+		URI modelURI = URI.createURI(newfile);
+		String diagramFile = filename.substring(0, filename.lastIndexOf("."))
+				+ ".problem_diagram";
+		URI diagramURI = URI.createURI(diagramFile);
+		ProblemDiagramEditorUtil.createDiagram(diagramURI, modelURI,
+				new NullProgressMonitor());
+
+		Resource xmiResource = resourceSet.createResource(modelURI);
 		xmiResource.getContents().add(xtextResource.getContents().get(0));
 		try {
 			xmiResource.save(null);
