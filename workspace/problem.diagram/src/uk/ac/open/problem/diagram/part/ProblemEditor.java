e1057ec095a7f361341c51a9cbe5ea61eaccb758
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-04-04 22:52:56 +0000

    create a main method so that the ProblemEditor can be called without the Eclipse GUI.
    In this mode, an EMF model can be automatically-generated from the .pf file.
    However, it is yet unclear whether the GMF diagram can be generated as well
    without invoking the GMF editor.
    
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@43 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- workspace/problem.diagram/src/uk/ac/open/problem/diagram/part/ProblemEditor.java
+++ workspace/problem.diagram/src/uk/ac/open/problem/diagram/part/ProblemEditor.java
@@ -1,6 +1,8 @@
 package uk.ac.open.problem.diagram.part;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.eclipse.core.resources.IFile;
@@ -11,14 +13,16 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.SaveAsDialog;
+import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.core.editor.XtextEditor;
 
 import uk.ac.open.ProblemStandaloneSetup;
+import uk.ac.open.parser.antlr.ProblemParser;
 
 /**
  * 
@@ -27,10 +31,26 @@ import uk.ac.open.ProblemStandaloneSetup;
  */
 public class ProblemEditor extends XtextEditor {
 
+	public static void main(String args[]) {
+		String filename = args[0];
+		// ProblemParser p = new ProblemParser();
+		// try {
+		// IParseResult result = p.parse("ProblemDiagram", new
+		// FileInputStream(new File(filename)));
+		saveModel(filename);
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// }
+	}
+
 	@Override
 	public void performSave(boolean overwrite, IProgressMonitor monitor) {
 		super.performSave(overwrite, monitor);
 		String filename = this.getResource().getFullPath().toOSString();
+		saveModel(filename);
+	}
+
+	private static void saveModel(String filename) {
 		// http://www.eclipse.org/forums/index.php?t=msg&goto=520616&
 		ProblemStandaloneSetup.doSetup();
 		XtextResourceSet resourceSet = new XtextResourceSet();
@@ -45,25 +65,29 @@ public class ProblemEditor extends XtextEditor {
 		String diagramFile = filename.substring(0, filename.lastIndexOf("."))
 				+ ".problem_diagram";
 		URI diagramURI = URI.createURI(diagramFile);
-		IPath path = new Path(diagramURI.toString()); 
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path); 
-		if (file!=null && file.exists()) { 		
-			SaveAsDialog fd = new SaveAsDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell());
-			fd.setOriginalName(file.getName());
-			fd.setTitle("Please specify the diagram name to save!");
-			fd.open();
-			path = fd.getResult();
-			if (path != null) {
-//				diagramURI = URI.createURI(filename.substring(0, filename.lastIndexOf("/")) + "/" + path.lastSegment());
-				diagramURI = URI.createURI(path.toOSString());
-			} else {
-				diagramURI = null;
+		IPath path = new Path(diagramURI.toString());
+		try {
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+			if (file != null && file.exists()) {
+				SaveAsDialog fd = new SaveAsDialog(PlatformUI.getWorkbench()
+						.getDisplay().getActiveShell());
+				fd.setOriginalName(file.getName());
+				fd.setTitle("Please specify the diagram name to save!");
+				fd.open();
+				path = fd.getResult();
+				if (path != null) {
+					diagramURI = URI.createURI(path.toOSString());
+				} else {
+					diagramURI = null;
+				}
 			}
+		} catch (IllegalStateException e) {
 		}
 		if (diagramURI != null)
 			ProblemDiagramEditorUtil.createDiagram(diagramURI, modelURI,
-				new NullProgressMonitor());
-		Resource xmiResource = resourceSet.createResource(modelURI);
+					new NullProgressMonitor());
+		Resource xmiResource = new XMIResourceFactoryImpl()
+				.createResource(modelURI);
 		xmiResource.getContents().add(xtextResource.getContents().get(0));
 		try {
 			xmiResource.save(null);
