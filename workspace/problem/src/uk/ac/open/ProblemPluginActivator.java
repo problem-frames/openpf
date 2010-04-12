25a39151566cacfab8cdf10acaa4b3c9c76c7390
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-04-12 08:58:44 +0000

    minor changes
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@65 650c8b6b-ff4b-431b-9fdd-e90af81125e7

---
+++ workspace/problem/src/uk/ac/open/ProblemPluginActivator.java
@@ -0,0 +1,11 @@
+package uk.ac.open;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class ProblemPluginActivator extends AbstractUIPlugin {
+
+	public ProblemPluginActivator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
