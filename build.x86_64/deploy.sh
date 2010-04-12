0caccd49f767f9336513bae91c5ec678c26e1b14
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-04-12 12:21:00 +0000

    deploy to openre folder instead of pf folder
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@69 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/deploy.sh
+++ build.x86_64/deploy.sh
@@ -2,5 +2,5 @@
 #-----------------------------------------------------------
 PRODUCT=pf
 BUILD_HOME=$HOME/eclipse.build
-TARGET_HOME=sead1:/home/share/sead/pf
+TARGET_HOME=sead1:/home/share/sead/openre
 scp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
