ecb0aa7b4dcc7f2baabcd054e8d4b16b613c01e4
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-08-25 13:07:59 +0000

    + modify the build script to tag the deployed product + modify the example of situation according to the new syntax - TODO: need to add more realistic ATM examples
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@122 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/eclipse.sh
+++ build.x86_64/eclipse.sh
@@ -5,4 +5,4 @@ export ECLIPSE_SDK=$HOME/IDE/sdk/$VERSION
 export ECLIPSE_HOME=$ECLIPSE_SDK/eclipse
 rm -rf configuration
 cp -rf $ECLIPSE_HOME/configuration .
-$ECLIPSE_SDK/eclipse/eclipse -plugincustomization plugin_customization.ini -configuration configuration -data ../workspace
+$ECLIPSE_SDK/eclipse/eclipse -plugincustomization plugin_customization.ini -configuration configuration -data `pwd`/../workspace
