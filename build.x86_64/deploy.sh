9bc54ee9b4913fe7c2b36dc9ea4b7123872929c1
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-09-09 23:45:25 +0000

    deploying on sead1 by continuous integration
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@127 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/deploy.sh
+++ build.x86_64/deploy.sh
@@ -2,6 +2,6 @@
 #-----------------------------------------------------------
 PRODUCT=pf
 BUILD_HOME=$HOME/eclipse.build
-TARGET_HOME=sead1:/home/share/sead/openre
-scp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
+TARGET_HOME=/home/share/sead/openre
+cp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
 svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
