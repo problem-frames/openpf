79224cf8a1859cf26ed3567b6af30dc49abb0d1f
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-09-09 23:50:24 +0000

    since continuous integration, the tagging will be too muchwq
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@128 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/deploy.sh
+++ build.x86_64/deploy.sh
@@ -4,4 +4,4 @@ PRODUCT=pf
 BUILD_HOME=$HOME/eclipse.build
 TARGET_HOME=/home/share/sead/openre
 cp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
-svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
+#svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
