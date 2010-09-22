727d4209f8f1658d91dce7d9dd3d98c33cfb231e
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-09-22 09:34:57 +0000

    import source code for the decreasor
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@152 650c8b6b-ff4b-431b-9fdd-e90af81125e7

---
+++ decreasoner/makeminisat_linux.sh
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2005 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Common Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/cpl-v10.html
+#
+# Contributors: 
+# IBM - Initial implementation
+#
+cd software
+unzip MiniSat_v1.14_src.zip
+cd MiniSat_v1.14
+make rs
+mv minisat_static ../../solvers
