6c1bdcc1edf81baed82422749ba7db99288f975e
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-09-24 00:08:01 +0000

    + usability  + Use the style of STRING to highlight the node names  + Outline of Problem editor uses more meaningful icons and labels
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@167 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- workspace/problem.ui/src/uk/ac/open/ui/ProblemUiModule.java
+++ workspace/problem.ui/src/uk/ac/open/ui/ProblemUiModule.java
@@ -4,6 +4,7 @@
 package uk.ac.open.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -12,4 +13,10 @@ public class ProblemUiModule extends uk.ac.open.ui.AbstractProblemUiModule {
 	public ProblemUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
 	}
+    /**
+     * @return ProblemSemanticHighlightingCalculator
+     */
+    public Class<? extends ISemanticHighlightingCalculator> bindSemanticHighlightingCalculator() {
+            return ProblemSemanticHighlightingCalculator.class;
+    }    
 }
