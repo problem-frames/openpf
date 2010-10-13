08b0cdd01dc778e9277ec59c160e0a94cf94b268
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-13 21:26:20 +0000

    + Fixed the SWTbot headless script    - TODO: install the headless script to the CI process
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@215 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- test/build-test.sh
+++ test/build-test.sh
@@ -7,3 +7,5 @@ rm -f $HOME/IDE/pf-3.6.product/retain/eclipse/plugins/problem_*.jar
 cp problem_*.jar $HOME/IDE/pf-3.6.product/eclipse/plugins
 cp problem_*.jar $HOME/IDE/pf-3.6.product/retain/eclipse/plugins
 cd -
+rm -rf $HOME/workspace
+eant
