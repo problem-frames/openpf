9a24f8b68eeb2017222267810425523b21c0f5dd
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-15 22:39:57 +0000

    + update the script for the updatesite
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@229 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/publish.sh
+++ build.x86_64/publish.sh
@@ -3,10 +3,13 @@
 ECLIPSE_HOME=$HOME/IDE/pf-3.6.product/eclipse
 SITEXML=file:/$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml 
 BUILT_REPO=$HOME/eclipse.build/update-3.6
+PUBLIC_REPO=/home/share/sead/pf/update
 CATEGORYXML=file:/$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category.xml 
-
+rm -rf $PUBLIC_REPO
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $SITEXML -configs cocoa.macosx.x86_64 -configs gtk.linux.x86_64 -compress -publishArtifacts
 
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $BUILD_REPO -configs cocoa.macosx.x86_64 -compress -publishArtifacts
 
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.CategoryPublisher -metadataRepository file://home/share/sead/pf/update -categoryDefinition $CATEGORYXML -categoryQualifier -compress
+cp -r $BUILT_REPO/features $PUBLIC_REPO 
+cp -r $BUILT_REPO/plugins $PUBLIC_REPO 
