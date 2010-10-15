508a02ac9c59d5a67504ab9600dbbbd968a0aac8
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-15 21:03:31 +0000

    + modify the pf.product for the build script
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@225 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/deploy.sh
+++ build.x86_64/deploy.sh
@@ -4,4 +4,5 @@ PRODUCT=pf
 BUILD_HOME=$HOME/eclipse.build
 TARGET_HOME=/home/share/sead/openre
 cp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
+./publish.sh
 #svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
