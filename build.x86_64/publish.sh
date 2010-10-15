923930d52d8403001119be6e0525f5e60c6b97ee
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-15 23:44:18 +0000

    + reorganise folder of documentations a bit
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@232 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/publish.sh
+++ build.x86_64/publish.sh
@@ -5,6 +5,7 @@ SITEXML=file:/$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/prob
 BUILT_REPO=$HOME/eclipse.build/update-3.6
 PUBLIC_REPO=/home/share/sead/pf/update
 CATEGORYXML=file:/$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category.xml 
+USERGUIDE=$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/doc/UserGuide
 rm -rf $PUBLIC_REPO
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $SITEXML -configs cocoa.macosx.x86_64 -configs gtk.linux.x86_64 -compress -publishArtifacts
 
@@ -13,3 +14,4 @@ java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.co
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.CategoryPublisher -metadataRepository file://home/share/sead/pf/update -categoryDefinition $CATEGORYXML -categoryQualifier -compress
 cp -r $BUILT_REPO/features $PUBLIC_REPO 
 cp -r $BUILT_REPO/plugins $PUBLIC_REPO 
+cp $USERGUIDE $PUBLIC_REPO
