ba402425503a91bcbbbd2b198e9c0214c70d2e31
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-25 21:12:45 +0000

    + fixed both the tickets #21 and #22
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@243 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- workspace/core/src/edu/toronto/cs/openome/core/action/CreateExamplesAction.java
+++ workspace/core/src/edu/toronto/cs/openome/core/action/CreateExamplesAction.java
@@ -10,6 +10,10 @@ package edu.toronto.cs.openome.core.action;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Vector;
 
 import org.eclipse.core.resources.IFile;
@@ -35,7 +39,9 @@ public class CreateExamplesAction extends ExtensionReader implements
 
 	public CreateExamplesAction() {
 	}
+
 	Vector<IConfigurationElement> examples = null;
+
 	public void run(IAction action) {
 		IWorkspace w = ResourcesPlugin.getWorkspace();
 		IProject project = w.getRoot().getProject("Examples");
@@ -45,58 +51,90 @@ public class CreateExamplesAction extends ExtensionReader implements
 			project.open(null);
 			examples = new Vector<IConfigurationElement>();
 			readExtensions("core.example", "example", examples);
-			for (IConfigurationElement example: examples) {
-				String pluginname = example.getDeclaringExtension().getNamespaceIdentifier();
+			for (IConfigurationElement example : examples) {
+				String pluginname = example.getDeclaringExtension()
+						.getNamespaceIdentifier();
 				Bundle bundle = Platform.getBundle(pluginname);
 				String filename = example.getAttribute("file");
-				String name = pluginname + "/" + filename;
-				String[] words = name.split("/");
-				String pathName = "";
-				IFolder folder = null;
-				for (int i = 0; i < words.length - 1; i++) {
-					String string = words[i];
-					pathName = pathName + string + "/";
-					folder = project.getFolder(pathName);
-					try {
-						if (!folder.exists())
-							folder.create(IResource.FORCE, true, null);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+				HashSet<String> more = fetchFile(project, pluginname, bundle, filename);
+				for (String f: more) {
+					// don't do recursion because it can be endless
+					// just one level of indirection already gives us enough flexibility :-)
+					fetchFile(project, pluginname, bundle, f);
 				}
-				InputStream stream = null;
-				Path path = new Path("samples/" + filename);
-				try {
-					stream = FileLocator.openStream(bundle, path, false);
-				} catch (IOException e) {
-					path = new Path(filename);
-					stream = FileLocator.openStream(bundle, path, false);
-				}
-				IFile file;
-				if (folder == null)
-					file = project.getFile(name);
-				else
-					file = folder.getFile(words[words.length - 1]);
-				try {
-					if (!file.exists())
-						file.create(stream, false, null);
-					if (filename.startsWith("bin")) {
-						ResourceAttributes attributes = new ResourceAttributes();
-						attributes.setExecutable(true);
-						file.setResourceAttributes(attributes);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				if (stream!=null)
-					stream.close();
-			}		
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (CoreException e) {
 			e.printStackTrace();
 		}
 		System.out.println("Example project created");
+	}
+
+	private HashSet<String> fetchFile(IProject project, String pluginname,
+			Bundle bundle, String filename) throws IOException {
+		HashSet<String> more = new HashSet<String>();
+		String name = pluginname + "/" + filename;
+		String[] words = name.split("/");
+		String pathName = "";
+		IFolder folder = null;
+		for (int i = 0; i < words.length - 1; i++) {
+			String string = words[i];
+			pathName = pathName + string + "/";
+			folder = project.getFolder(pathName);
+			try {
+				if (!folder.exists())
+					folder.create(IResource.FORCE, true, null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		InputStream stream = null;
+		Path path = new Path("samples/" + filename);
+		try {
+			stream = FileLocator.openStream(bundle, path, false);
+		} catch (IOException e) {
+			path = new Path(filename);
+			try {
+				stream = FileLocator.openStream(bundle, path, false);
+			} catch (IOException e2) {
+				try {
+					URL url = new URL(filename);
+					URLConnection uc = url.openConnection();
+					stream = uc.getInputStream(); // ignore it!
+				} catch (Exception x) {
+					stream = null;
+				}
+			}
+		}
+		if (stream != null) {
+			IFile file;
+			if (folder == null)
+				file = project.getFile(name);
+			else
+				file = folder.getFile(words[words.length - 1]);
+			try {
+				if (!file.exists())
+					file.create(stream, false, null);
+				if (filename.startsWith("bin")) {
+					ResourceAttributes attributes = new ResourceAttributes();
+					attributes.setExecutable(true);
+					file.setResourceAttributes(attributes);
+				}
+				if (filename.endsWith(".dir")) {
+					Scanner sc = new Scanner(stream);
+					while (sc.hasNextLine()) {
+						String s = sc.nextLine();
+						System.out.println(s);
+						more.add(s);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			stream.close();
+		}
+		return more;
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
