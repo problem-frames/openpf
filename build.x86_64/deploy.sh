d7f30bf975f45e11d1bbc06c4edab420f6695e12
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2010-10-28 05:09:06 +0000

    + restore the tagging command in build script
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@246 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/deploy.sh
+++ build.x86_64/deploy.sh
@@ -5,4 +5,4 @@ BUILD_HOME=$HOME/eclipse.build
 TARGET_HOME=/home/share/sead/openre
 cp $BUILD_HOME/I.$PRODUCT/*.zip $TARGET_HOME
 ./publish.sh
-#svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
+svn copy $repo/openpf/trunk $repo/openpf/tags/`date +%d-%m-%Y` -m "deployed"
