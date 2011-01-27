4e8d96ffb23c796f37dedc5669c0fefde915c4a3
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2011-01-27 09:23:20 +0000

    + join the script
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@355 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/publish.sh
+++ build.x86_64/publish.sh
@@ -7,8 +7,7 @@ BUILT_REPO=$HOME/eclipse.build/update-3.6
 PUBLIC_REPO=/home/share/sead/openre/update
 USERGUIDE=$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/doc/UserGuide
 rm -rf $PUBLIC_REPO
-awk -f version.awk
-$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml
+awk -f version.awk $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml
 awk -f version.awk $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category.xml
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $SITEXML -configs cocoa.macosx.x86_64 -configs gtk.linux.x86_64 -compress -publishArtifacts
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $BUILD_REPO -configs cocoa.macosx.x86_64 -compress -publishArtifacts
