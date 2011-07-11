81557c4890c82d9f88d59a774b34146cc5c5e575
yy66 <yy66@650c8b6b-ff4b-431b-9fdd-e90af81125e7>
2011-07-11 18:29:31 +0000

    + update the bash script to  generate the appropriate site.xml
    git-svn-id: https://computing-research.open.ac.uk/repos/openpf/trunk@402 650c8b6b-ff4b-431b-9fdd-e90af81125e7

--- build.x86_64/publish.sh
+++ build.x86_64/publish.sh
@@ -10,7 +10,7 @@ USERGUIDE=$HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/doc/UserGuide
 function publish() {
 rm -rf $PUBLIC_REPO
 awk -f version.awk $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml
-awk -f version.awk $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category.xml
+awk -f version.awk $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site-qualifier.xml | sed -e 's/\.\./.qualifier./g' | sed -e 's/\."/.qualifier"/g' > $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/category.xml
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $SITEXML -configs cocoa.macosx.x86_64 -configs gtk.linux.x86_64 -compress -publishArtifacts
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher -metadataRepository file://home/share/sead/pf/update -artifactRepository file://home/share/sead/pf/update -source $BUILD_REPO -configs cocoa.macosx.x86_64 -compress -publishArtifacts
 java -cp $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.core.launcher.Main -application org.eclipse.equinox.p2.publisher.CategoryPublisher -metadataRepository file://home/share/sead/pf/update -categoryDefinition $CATEGORYXML -categoryQualifier -compress
@@ -20,15 +20,17 @@ cp -r $BUILT_REPO/plugins $PUBLIC_REPO
 cp $USERGUIDE $PUBLIC_REPO
 }
 publish
-list=$(grep qualifier $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml \
-| awk '{split($2, a, /\//); split(a[2], b, /\./); print b[1]}')
+list=$(grep "feature url=" $HOME/build/cruisecontrol-bin-2.8.3/projects/openpf/workspace/problem.update/site.xml \
+| awk '{split($2, a, /\//); n=split(a[2], b, /_/); print b[1];}')
+#echo $list
 cp $PUBLIC_REPO/site.xml .
 for l in $list; do
-  if [ -e $BUILT_REPO/features/$l*.jar ];  then
-	jar=$(basename $BUILT_REPO/features/$l*.jar)
+  if [ -e $BUILT_REPO/features/"$l"_*.jar ];  then
+	jar=$(basename $BUILT_REPO/features/"$l"_*.jar)
   	jar2=${jar/.jar/}
-  	jar3=${jar2/*1.0.0./}
-	sed "s/$l.feature_1.0.0.qualifier.jar\" id=\"$l.feature\" version=\"1.0.0.qualifier/$jar\" id=\"$l.feature\" version=\"1.0.0.$jar3/" site.xml > site.xml.new
+  	jar3=${jar2#*_}
+#        echo $jar3
+	sed "s/"$l"_.*\" id=\""$l"\" version=\".*\"/$jar\" id=\""$l"\" version=\"$jar3\"/" site.xml > site.xml.new
 	mv site.xml.new site.xml
   fi
 done
